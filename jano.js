a31d27ce133ee3c57afb41d278e6cf4d799af61f
Lawand2000 <93160041+Lawand2000@users.noreply.github.com>
2022-06-10 21:31:46 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -1907,9 +1907,9 @@ jano100.on("ready", () => {
 
 //////////////// tokn lera da bne
 
-jano.login("OTM0MDUwNDgyNDc1OTAwOTc4.YeqdUA.w-BLGG_1gcvB5Df1Z9LdGiPN_2Y");
-jano2.login("OTM0MDUzNDc1MTU3OTM4MTg3.Yeqfqw.aKZVpQnl-66rqU8ShbzKKy8SgUc");
-jano3.login("OTQxMDgxNTQzMzAzMTI3MDYw.YgQxww.wtdGW4ixEVQyWjOcZ6_0HnUFspc");
+jano.login("OTQxMDgxNTQzMzAzMTI3MDYw.YgQxww.wtdGW4ixEVQyWjOcZ6_0HnUFspc");
+jano2.login("");
+jano3.login("");
 jano4.login("");
 jano5.login("");
 jano6.login("");
